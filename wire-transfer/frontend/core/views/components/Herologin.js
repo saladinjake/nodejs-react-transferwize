5b1d6dcaaee54e18f8f5d9bea55f550b1d03947c
saladinjake <juwavictor@gmail.com>
2022-02-18 03:00:25 +0100

    ch(update package json file)

--- wire-transfer/frontend/core/views/components/Herologin.js
+++ wire-transfer/frontend/core/views/components/Herologin.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react"
 import {
   Flex,
   Box,
@@ -14,13 +15,31 @@ import {
 } from '@chakra-ui/react';
 
 export default function Login() {
-  return (
-    <Flex
-      minH={'100vh'}
+  const [showLogin, setShowLogin] = useState(true);
+  const [showRegister, setShowRegister] = useState(false);
 
+  const handleRegisterDisplay = (e) =>{
+    e.preventDefault()
+     setShowLogin(false)
+    setShowRegister(true)
+
+  }
+
+
+  const handleLoginDisplay = (e) =>{
+    e.preventDefault()
+
+     setShowLogin(true)
+    setShowRegister(false)
+  }
+  return (
+    <>
+    <Flex
+      display={showLogin? "block": "none"}
+      minH={'100vh'}
       align={'center'}
       justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
+      >
       <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
         <Stack align={'center'}>
           <Heading fontSize={'4xl'}>TRANSFER WIZ</Heading>
@@ -49,7 +68,7 @@ export default function Login() {
                 align={'start'}
                 justify={'space-between'}>
                
-                <Link color={'blue.400'}>Don't have an account yet?Sign up.</Link>
+                <Link onClick={(e)=>{handleRegisterDisplay(e)}} color={'blue.400'}>Don't have an account yet?Sign up.</Link>
               </Stack>
               <Button
                 bg={'blue.400'}
@@ -64,5 +83,79 @@ export default function Login() {
         </Box>
       </Stack>
     </Flex>
+
+
+
+
+
+
+
+    <Flex
+    display={showRegister? "block": "none"}
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      >
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>TRANSFER WIZ</Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            Sign Up<Link color={'blue.400'}></Link> 
+          </Text>
+        </Stack>
+        <Box
+         minW={'500px'}
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>First Name</FormLabel>
+              <Input type="text"  name="firstname"/>
+            </FormControl>
+
+            <FormControl id="email">
+              <FormLabel>Last Name</FormLabel>
+              <Input type="text" name="lastname" />
+            </FormControl>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input name="email" type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+
+             <FormControl id="password">
+              <FormLabel>Confirm Password</FormLabel>
+              <Input name="confirmpassword" type="password" />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+               
+                <Link onClick={(e)=>{handleLoginDisplay(e)}} color={'blue.400'}>Already have an account?Sign in.</Link>
+              </Stack>
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Sign up
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+
+
+
+    </>
   );
 }
--- wire-transfer/frontend/pages/_app.js
+++ wire-transfer/frontend/pages/_app.js
@@ -4,16 +4,26 @@ import React from "react"
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider as Provisioner }  from "react-redux";
 import QReduxStore from "../core/redux/store";
+import { LoadingProgressProvider } from '../core/views/components/Loading'
 
 export default function App({ Component, pageProps }) {
     return (
 
    <>
+    <NextNprogress
+          color="linear-gradient(90deg, rgba(0,61,89, 0.6) 33%, rgba(67,178,99,1) 67%);"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+        />
      {/*REDUX STATE ENHANCER*/}
     <Provisioner store={QReduxStore}>
-          <ChakraProvider>
+        <ChakraProvider>
+          <LoadingProgressProvider>
                   <Component {...pageProps} />
-            </ChakraProvider>
+          </LoadingProgressProvider>
+        </ChakraProvider>
     </Provisioner>
   </>
     
